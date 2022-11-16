e11c59cb7990333ce85bb655327b7da07b594b8a
Akachukwu <105462943+Akachukwu96@users.noreply.github.com>
2022-11-16 18:58:53 +0100

    Update _atoi.c
    atoi

--- _atoi.c
+++ _atoi.c
@@ -4,7 +4,7 @@
  * interactive - returns true if shell is interactive mode
  * @info: struct address
  *
- * Return: 1 if interactive mode, 0 otherwise
+ * Return: 1 if interactive mode, 0 if otherwise
  */
 int interactive(info_t *info)
 {
@@ -15,6 +15,7 @@ int interactive(info_t *info)
  * is_delim - checks if character is a delimeter
  * @c: the char to check
  * @delim: the delimeter string
+ *
  * Return: 1 if true, 0 if false
  */
 int is_delim(char c, char *delim)
@@ -28,6 +29,7 @@ int is_delim(char c, char *delim)
 /**
  *_isalpha - checks for alphabetic character
  *@c: The character to input
+ *
  *Return: 1 if c is alphabetic, 0 otherwise
  */
 
